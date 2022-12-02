2fcef74498581fb51c32fa94c3f9a65e006d5d74
chasomin <somin49@naver.com>
2022-12-02 10:14:09 +0900

    add

---
+++ Git/GitApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitApp.swift
+//  Git
+//
+//  Created by 차소민 on 2022/12/02.
+//
+
+import SwiftUI
+
+@main
+struct GitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
